e04aa624d3c07e2a30c8c5326d0481544f968738
Ketan Singh <singhketan987@gmail.com>
2020-09-28 22:52:49 +0530

    index and style updated

--- js/index.js
+++ js/index.js
@@ -1,32 +1,61 @@
 const app = document.getElementById("app");
-
-const listData = ["hi", "he"];
+ 
+const listData = [];
 
 const listWrapper = document.createElement("div");
+
 const addBtn = document.createElement("button");
 addBtn.innerHTML = "add new todo";
+
 const orderList = document.createElement("ol");
+
+var input = document.createElement("input");
+input.setAttribute('type', 'text');
 
 const printData = () => {
   for (let i = 0; i < listData.length; i++) {
-    addData(listData[i]);
+    const list = document.createElement("li");
+    list.appendChild(document.createTextNode(listData[i]));
+    listWrapper.appendChild(list);
   }
 };
 
-const addData = (listText) => {
-  const list = document.createElement("li");
-  list.appendChild(document.createTextNode(listText));
-  orderList.appendChild(list);
-};
+var close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
 addBtn.onclick = () => {
-  const userData = "data by user";
-  listData.push(userData);
-  addData(userData);
+  const list = document.createElement("li");
+  const userData = input.value;
+  if(userData === ''){
+    alert("You must write something!");
+  }
+  else{
+    list.appendChild(document.createTextNode(userData));
+    listData.push(userData);
+    listWrapper.appendChild(list); 
+  }
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  list.appendChild(span);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 };
 
 printData();
 
-listWrapper.append(orderList);
+//listWrapper.append(orderList);
+app.append("Enter task:", input);
 app.append(listWrapper);
 app.append(addBtn);
