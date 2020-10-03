b6185220d1e4be887eda4529bcd3ed43e3fa4f80
shail sheth <shailysheth@yahoo.com>
2020-10-03 13:50:05 +0530

    modal changes

--- js/index.js
+++ js/index.js
@@ -24,13 +24,33 @@ const clearUI = () => {
   orderList.innerHTML = "";
 };
 
-addBtn.onclick = () => {
-  const userData = "data by user";
-  clearUI();
-  listData.push(userData);
-  printData();
-};
 
+// Get the modal
+const modal = document.getElementById("myModal");
+
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+addBtn.onclick = function() {
+modal.style.display = "none";
+clearUI();
+listData.push(userData);
+printData();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 printData();
 
 listWrapper.append(orderList);
