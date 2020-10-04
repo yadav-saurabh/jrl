d6584f4bab63b332c53cb0fa1892dd2f48dfcb94
yadav-saurabh <yadav-saurabh@outlook.com>
2020-10-04 10:05:18 +0530

    reviewed

--- js/index.js
+++ js/index.js
@@ -1,46 +1,65 @@
+/**
+ * overall -
+ * - proper naming for variable
+ * - logic can be simplified
+ */
 
-const addTodoModal = document.getElementById('myModal');
-const userInputs = addTodoModal.querySelectorAll('input');
+const addTodoModal = document.getElementById("myModal");
+const userInputs = addTodoModal.querySelectorAll("input");
 const toDoListItems = [];
 
-let modal = document.getElementById('myModal');
-let btn = document.getElementById('myBtn');
-let span = document.getElementsByClassName('close')[0];
+let modal = document.getElementById("myModal");
+/** use proper naming for variable */
+let btn = document.getElementById("myBtn");
+/** use proper naming for variable
+ *  use getElementsByClassName only if you want all elements sy that calss for a single element use id
+ */
+let span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
-  modal.style.display = 'block';
+  modal.style.display = "block";
 };
 
 span.onclick = function () {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 };
 
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 };
 
 const renderNewModalEl = (id, title, desc) => {
-  const ol = document.getElementById('todo-list');
-  const newModalEl = document.createElement('li');
+  /** use proper naming for variable */
+  const ol = document.getElementById("todo-list");
+  const newModalEl = document.createElement("li");
   newModalEl.textContent = ` Title : ${title}    Desc : ${desc}`;
   ol.append(newModalEl);
 
-  const button = document.createElement('button');
-  button.innerHTML = 'Delete ToDo';
-  button.setAttribute('class', 'delBtn--danger');
+  const button = document.createElement("button");
+  button.innerHTML = "Delete ToDo";
+  button.setAttribute("class", "delBtn--danger");
   ol.append(button);
 
+  /** 
+   * no need for defining deleteListHandle here
+   * check this: -  
+   *  button.addEventListener('click', () => {....rest of the code}
+   * or simply
+   * button.onclick = () => {...rest of the code}
+   * 
+   * logic can be simplified
+   */
   /* action to perform when clicked on delete todo */
   button.addEventListener(
-    'click',
+    "click",
     (deleteListHandle = () => {
       let arrayId;
       toDoListItems.map(function (el) {
         arrayId = el.id;
         if (id === arrayId) {
-          removeByAttr(toDoListItems, 'id', id);
+          removeByAttr(toDoListItems, "id", id);
         }
       });
       ol.removeChild(newModalEl);
@@ -52,22 +71,26 @@ const renderNewModalEl = (id, title, desc) => {
   let removeByAttr = function (toDoListItems, attr, value) {
     let i = toDoListItems.length;
     while (i--) {
-      if (toDoListItems[i]
-        && toDoListItems[i].hasOwnProperty(attr)
-        && (arguments.length > 2 && toDoListItems[i][attr] === value)) {
+      if (
+        toDoListItems[i] &&
+        toDoListItems[i].hasOwnProperty(attr) &&
+        arguments.length > 2 &&
+        toDoListItems[i][attr] === value
+      ) {
         toDoListItems.splice(i, 1);
       }
     }
     return toDoListItems;
-  }
+  };
 };
 
 /* To clear inputs of title and description after add todo is clicked*/
 const clearTodoInput = () => {
   for (const usrInput of userInputs) {
-    usrInput.value = '';
+    usrInput.value = "";
   }
 };
+/** don't leave commented code */
 /*
 const closeTodoModal = () => {
   addTodoModal.classList.remove('visible');
@@ -77,8 +100,16 @@ const closeTodoModal = () => {
 const addListHandler = () => {
   const title = userInputs[0].value;
   const desc = userInputs[1].value;
-  if (title === '' || desc === '') {
-    alert('please enter a valid title and desc');
+  /**
+   * check this:-
+   * if(!title && !desc) {
+   *   return alert('value not valid');
+   * }
+   * ..... rest of the code with no else
+   *
+   */
+  if (title === "" || desc === "") {
+    alert("please enter a valid title and desc");
   } else {
     const newTodo = {
       id: Math.random().toString(),
@@ -87,10 +118,14 @@ const addListHandler = () => {
     };
     toDoListItems.push(newTodo);
     clearTodoInput();
+    /** don't leave commented code
+     * you can pass the object as a params instead of single values of the params
+     * renderNewModalEl(newTodo);
+     */
     // closeTodoModal();
     /* to pass array elements to list */
     renderNewModalEl(newTodo.id, newTodo.title, newTodo.desc);
   }
 };
-const addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', addListHandler);
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", addListHandler);
